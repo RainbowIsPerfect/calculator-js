18cacffc48214a3cbf4a3c28b90ec5b34ad88350
RainbowIsPerfect <andrey-smollin@rambler.ru>
2022-12-08 03:30:22 +0300

    update_2

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const buttonItems = Array.from(buttonsList.children);
 const borderRadiusButton = document.querySelector('#border-radius-button');
 const colorButton = document.querySelector('#output-color-button');
 const dotButton = document.querySelector('.button-dot');
+const textAlignButton = document.querySelector('#output-text-align-button');
+
 
 const numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const operatorsArray = ['+', '-', '/', '*', '%'];
@@ -13,33 +15,32 @@ let numOne = '';
 let numTwo = '';
 let operator = '';
 
-output.addEventListener('DOMCharacterDataModified', function(){
-    console.log('changed');
-})
-
 buttonItems.forEach(function (button) {
     button.addEventListener('click', function () {
         if (this.innerHTML == 'C') {
             clear();
-        } else if (this.innerHTML == 'del') {
+        } else if (this.innerHTML == 'CE') {
             if (numTwo == '') {
                 numOne = numOne.slice(0, -1);
-                output.innerHTML = numOne;
+                output.setAttribute('value', numOne);
                 operator = '';
             } else {
                 numTwo = numTwo.slice(0, -1);
-                output.innerHTML = numTwo;
+                output.setAttribute('value', numTwo);
             }
-            if (output.innerHTML == '') output.innerHTML = 0;
+            if (!numOne.includes('.') && !numTwo.includes('.')) {
+                dotButton.removeAttribute('disabled', '')
+            }
+            if (output.getAttribute('value') == '') output.setAttribute('value', 0);;
         } else if (numbersArray.includes(button.innerHTML)) {
             if (numOne != '' && operator != '') {
                 numTwo += button.innerHTML;
-                output.innerHTML = numTwo;
+                output.setAttribute('value', numTwo);
             } else {
                 numOne += button.innerHTML;
-                output.innerHTML = numOne;
+                output.setAttribute('value', numOne);
             }
-            if (output.innerHTML.includes('.')) {
+            if (output.getAttribute('value').includes('.')) {
                 dotButton.setAttribute('disabled', '');
             } else {
                 dotButton.removeAttribute('disabled', '')
@@ -49,29 +50,29 @@ buttonItems.forEach(function (button) {
         } else if (this.innerHTML == '=' && numTwo != '') {
             switch (operator) {
                 case '+':
-                    output.innerHTML = `${+numOne + +numTwo}`;
+                    output.setAttribute('value', +numOne + +numTwo);
                     break;
                 case '-':
-                    output.innerHTML = `${numOne - numTwo}`
+                    output.setAttribute('value', numOne - numTwo);
                     break;
                 case '*':
-                    output.innerHTML = `${numOne * numTwo}`
+                    output.setAttribute('value', numOne * numTwo);
                     break;
                 case '%':
-                    output.innerHTML = `${numOne % numTwo}`
+                    output.setAttribute('value', numOne % numTwo);
                     break;
                 case '/':
                     let i = (numOne / numTwo).toFixed(2).toString();
                     if (i[i.length - 1] == '0') {
                         i = i.replace(/.$/, '');
-                        output.innerHTML = i;
+                        output.setAttribute('value', i);
                         if (i[i.length - 1] == '0') {
                             i = i.replace(/..$/, '');
-                            output.innerHTML = i;
+                            output.setAttribute('value', i);
                         }
-                    } else output.innerHTML = i;
+                    } else output.setAttribute('value', i);
             }
-            numOne = output.innerHTML;
+            numOne = output.getAttribute('value');
             numTwo = '';
             operator = '';
         }
@@ -79,7 +80,8 @@ buttonItems.forEach(function (button) {
 })
 
 function clear() {
-    output.innerHTML = '0';
+    output.setAttribute('value', 0);
+    dotButton.removeAttribute('disabled', '')
     numOne = '';
     numTwo = '';
     operator = '';
@@ -93,4 +95,8 @@ borderRadiusButton.addEventListener('click', function () {
 
 colorButton.addEventListener('click', function () {
     output.classList.toggle('output-light');
+})
+
+textAlignButton.addEventListener('click', function () {
+    output.classList.toggle('output-txt');
 })
